5251468989593539d3ebf8e09c5cb1ede98bb890
Ezequiel Leanes <ezequiel.leanes@gmail.com>
2022-05-04 18:39:42 -0300

    Typo

--- src/components/PageContent/index.js
+++ src/components/PageContent/index.js
@@ -4,7 +4,7 @@ import './index.css';
 export function PageContent() {
   return (
     <Box className="content-container">
-      <Typography variant="h3" color="secondary" sx={{ pt: '60px', mb: '12px' }} align="center">DEFEDEMOS AL TRABAJADOR</Typography>
+      <Typography variant="h3" color="secondary" sx={{ pt: '60px', mb: '12px' }} align="center">DEFENDEMOS AL TRABAJADOR</Typography>
       <Typography variant="body1" color="secondary" align="center">Somos especialistas en derecho laboral, indemnización por despido, maltrato laboral, acoso laboral, trabajo en negro, enfermedades profesionales, diferencias salariales, horas extras impagas, discriminación laboral, trabajo insalubre y más.</Typography>
     </Box>
   );
